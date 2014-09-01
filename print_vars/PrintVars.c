1884cf90b9beb643d925a7a6b6b9de0022ae071b
Tejas Manohar <me@tejasmanohar.com>
2014-09-01 18:56:53 -0500

    compute arrays

---
+++ print_vars/PrintVars.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+  int num = 5;
+  float numTwo = 4.5;
+  double numThree = 5.52;
+  double test;
+  
+  printf("The first num is %d",num);
+  printf("The not defined var is %f", test);
+  return 0;
+}
+  
