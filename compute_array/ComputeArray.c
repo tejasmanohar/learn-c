ffba37e8af8657cb85e825f71b03100a8784cea2
Tejas Manohar <me@tejasmanohar.com>
2014-09-01 19:07:39 -0500

    fix nasty tabs

--- compute_array/ComputeArray.c
+++ compute_array/ComputeArray.c
@@ -5,10 +5,10 @@ int main() {
     int average;
     
       grades[0] = 80;
-               grades[2] = 90;
+        grades[2] = 90;
                
-                 average = (grades[0] + grades[1] + grades[2]) / 3;
-                   printf("The average of the 3 grades is: %d", average);
+           average = (grades[0] + grades[1] + grades[2]) / 3;
+             printf("The average of the 3 grades is: %d", average);
                    
-                     return 0;
-                     }
+               return 0;
+           }
