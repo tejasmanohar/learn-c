7325f2372576b05025fd77b977cb8e8e7235118c
Tejas Manohar <me@tejasmanohar.com>
2014-09-01 19:16:18 -0500

    increase by adding grades

--- compute_array/ComputeArray.c
+++ compute_array/ComputeArray.c
@@ -6,6 +6,7 @@ int main() {
     
       grades[0] = 80;
         grades[2] = 90;
+        grades[1] = 100;
                
            average = (grades[0] + grades[1] + grades[2]) / 3;
              printf("The average of the 3 grades is: %d", average);
